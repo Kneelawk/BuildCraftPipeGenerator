718aa56fb8bf210d6566e5c63388c23192f8b7d4
Kneelawk <jedidiah.pommert@gmail.com>
2016-12-17 13:08:26 -0800

    Begin on generator

--- src/main/scala/org/kneelawk/buildcraftpipegenerator/BuildCraftPipeGenerator.scala
+++ src/main/scala/org/kneelawk/buildcraftpipegenerator/BuildCraftPipeGenerator.scala
@@ -1,7 +1,16 @@
 package org.kneelawk.buildcraftpipegenerator
 
+import org.kneelawk.wavefronttools.ObjWriter
+import java.io.File
+import java.io.FileOutputStream
+
 object BuildCraftPipeGenerator {
   def main(args: Array[String]) {
+    val outputDir = new File("output")
+    if (!outputDir.exists()) outputDir.mkdirs()
     
+    val bigFile = new File(outputDir, "full.obj")
+    
+    val bigOut = new ObjWriter(new FileOutputStream(bigFile))
   }
 }
